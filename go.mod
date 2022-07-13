894db6fee52305f67cd0cd9898271c0428225294
jayaraman <jayaramanmuthupandianbe@gmail.com>
2022-07-13 16:05:30 +0530

    gorm-todo

--- go.mod
+++ go.mod
@@ -13,6 +13,4 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
-	gorm.io/driver/mysql v1.3.5 // indirect
-	gorm.io/gorm v1.23.8 // indirect
 )
--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	log "github.com/sirupsen/logrus"
+	"strconv"
+
 )
 
 var db, err = gorm.Open("mysql", "root:root123@tcp(localhost:33061)/todolist?charset=utf8&parseTime=True&loc=Local")
@@ -32,6 +34,69 @@ func CreateItem(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// getItemByID
+
+func GetItemByID(Id int ) bool {
+	todo := &TodoItemModel{}
+	result := db.First(&todo, Id)
+	if result.Error !=nil {
+		log.Warn("TodoItem not fount DB")
+		return false 
+	}
+	return true
+}
+
+
+// update
+
+func UpdateItem(w http.ResponseWriter, r *http.Request) {
+
+vars := mux.Vars(r)
+id, _ := strconv.Atoi(vars["id"])
+
+err := GetItemByID(id)
+if err ==false {
+	w.Header().Set("Content-type", "Application/json")
+	io.WriteString(w, `{deleted": false, "error": "not found"}`)
+} else {
+	completed, _ := strconv.ParseBool(r.FormValue("completed"))
+	log.WithFields(log.Fields{"Id" : id, "Completed": completed}). Info("Updating TodoItem")
+	todo := TodoItemModel{}
+	db.First(&todo, id)
+	todo.Completed = completed 
+	db.Save(&todo)
+	w.Header().Set("Content-type", "Application/json")
+	io.WriteString(w, `{"updated" : true}`)
+	
+}
+}
+
+// delete
+
+func DeleteItem(w http.ResponseWriter, r *http.Request) {
+
+	Vars:= mux.Vars(r)
+	id, _ := strconv.Atoi(Vars["id"])
+
+	err := GetItemByID(id)
+	if err == false {
+	w.Header().Set("Content-type", "Application/json")
+	io.WriteString(w, `{"deleted" : false, "error" : "not found"}`)
+
+} else {
+	log.WithFields(log.Fields{"Id" : id,}). Info("Deleting TodoItem")
+	todo := TodoItemModel{}
+	db.First(&todo, id)
+	db.Delete(&todo)
+	w.Header().Set("Content-type", "Application/json")
+	io.WriteString(w, `{"Deleted" : true}`)
+}
+}
+
+
+	
+
+
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
 	w.Header().Set("Content-Type", "application/json")
@@ -51,5 +116,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
 	router.HandleFunc("/todo", CreateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", UpdateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", DeleteItem).Methods("DELETE")
 	http.ListenAndServe(":8000", router)
 }
