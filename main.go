f4094af928941f2d929a73aa8b076eaa0326aba2
jayaraman <jayaramanmuthupandianbe@gmail.com>
2022-07-13 11:49:13 +0530

    first commit

--- main.go
+++ main.go
@@ -1,107 +1,55 @@
-// package main
-
-// import (
-// 	"io"
-// 	"net/http"
-
-// 	"github.com/gorilla/mux"
-// 	log "github.com/sirupsen/logrus"
-// 	//"github.com/jinzhu/gorm"
-// 	//"gorm.io/driver/postgres"
-// 	// "github.com/go-sql-driver/mysql"
-// 	//  "github.com/jinzhu/gorm"
-// 	//  "github.com/jinzhu/gorm/dialects/mysql"
-// )
-
-// //var db, _ := gorm.Open(postgres.Open("postgres://ecgjxqyi:p0AK5VS0f9j-WtbAeTfwU0AFM1Z3FUmQ@queenie.db.elephantsql.com/ecgjxqyi"),&gorm.Config{})
-
-// //var db, _ := gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
-
-// // type TodoItemModel struct{
-// // 		 Id int `gorm:"primary_key"`
-// // 		 Description string
-// // 		 Completed bool
-// // 	}
-
-// func Healthz(w http.ResponseWriter, r *http.Request) {
-// 	//  defer db.Close()
-// 	//  db.Debug().DropTableIfExists(&TodoItemModel{})
-// 	//  db.Debug().AutoMigrate(&TodoItemModel{})
-
-// 	log.Info("API Health is OK")
-// 	w.Header().Set("Content-Type", "application/json")
-// 	io.WriteString(w, `{"alive": true}`)
-// }
-
-// // func init() {
-// // 	log.SetFormatter(&log.TextFormatter{})
-// // 	log.SetReportCaller(true)
-// // }
-
-// func main() {
-// 	log.Info("Starting Todo API server")
-// 	router := mux.NewRouter()
-// 	router.HandleFunc("/healthz", Healthz).Methods("GET")
-// 	http.ListenAndServe(":8000", router)
-// }
-
-
 package main
 
 import (
-
-	//"io"
+	"io"
 	"net/http"
-	//"strconv"
 
+	"encoding/json"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	log "github.com/sirupsen/logrus"
 )
 
-// Healthz
+var db, err = gorm.Open("mysql", "root:root123@tcp(localhost:33061)/todolist?charset=utf8&parseTime=True&loc=Local")
 
-func Healthz(W http.ResponseWriter, r *http.Request) {
+type TodoItemModel struct {
+	Id          int `gorm:"primary_key"`
+	Description string
+	Completed   bool
+}
+
+func CreateItem(w http.ResponseWriter, r *http.Request) {
+	description := r.FormValue("description")
+	log.WithFields(log.Fields{"description": description}).Info("Add new TodoItem. Saving to database")
+	todo := &TodoItemModel{Description: description, Completed: false}
+	db.Create(&todo)
+	result := db.Last(&todo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result.Value)
 
 }
 
-// GetCompletedItems
-
-func GetCompletedItems(w http.ResponseWriter, r *http.Request) {
-
-}
-
-// GetIncompleteItems
-
-func GetIncompleteItems(W http.ResponseWriter, r *http.Request) {
-
-}
-
-// CreateItem
-
-func CreateItem(W http.ResponseWriter, r *http.Request) {
-
-}
-
-// UpdateItem
-
-func UpdateItem(W http.ResponseWriter, r *http.Request) {
-
-}
-
-// DeleteItem
-
-func DeleteItem(W http.ResponseWriter, r *http.Request){
-
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	log.Info("API Health is OK")
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"alive": true}`)
 }
 
 func main() {
 
+	defer db.Close()
+	if err != nil {
+		log.Error(err)
+	}
+	db.Debug().DropTableIfExists(&TodoItemModel{})
+	db.Debug().AutoMigrate(&TodoItemModel{})
+
+	log.Info("Starting Todolist API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
-	router.HandleFunc("/todo-completed", GetCompletedItems).Methods("GET")
-	router.HandleFunc("/todo-incomplete", GetIncompleteItems).Methods("GET")
 	router.HandleFunc("/todo", CreateItem).Methods("POST")
-	router.HandleFunc("/todo/{id}", UpdateItem).Methods("POST")
-	router.HandleFunc("/todo/{id}", DeleteItem).Methods("DELETE")
 	http.ListenAndServe(":8000", router)
 }
-
